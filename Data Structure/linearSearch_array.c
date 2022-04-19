044b1479c40ffd01c270cfe96b01fa74bee3ebfd
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-19 19:25:41 +0530

    Create linearSearch_array.c

---
+++ Data Structure/linearSearch_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int linearSearch(int arr[], int size, int element)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == element)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    int arr[] = {1, 3, 5, 56, 4, 3, 23, 5, 4, 56, 34};
+    int size = sizeof(arr) / sizeof(int);
+    int element = 56;
+    int searchIndex=linearSearch(arr,size,element);
+    printf("%d element was found at %d",element,searchIndex);
+
+    return 0;
+}
